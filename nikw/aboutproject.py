cd6752b7077028ddc51ce0d64947c2345bd42e92
suizokukan <suizokukan@orange.fr>
2022-04-20 11:47:57 +0200

    v. 0.0.0

---
+++ nikw/aboutproject.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+################################################################################
+#    nikw Copyright (C) 2021 suizokukan
+#    Contact: suizokukan _A.T._ orange dot fr
+#
+#    This file is part of nikw.
+#    nikw is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    nikw is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with nikw.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+"""
+    nikw project : wisteria/aboutproject.py
+
+    Global constants defining the project : version number, author, license, etc.
+    ___________________________________________________________________________
+
+    o __author__      : (str) project's initial author
+    o __email__       : (str) project's official email
+    o __license__     : (str) project's license
+    o __location__    : (str) project's official site
+    o __projectname__ : (str) Official project's name
+    o __version__     : (str) project's version
+                       To propagate the project's version across all the
+                       project, see the propagate_versionnumber.py script.
+"""
+
+__author__ = "Xavier Faure (suizokukan)"
+__email__ = "suizokukan@orange.fr"
+__license__ = "GPL-3.0"
+__location__ = "github.com/suizokukan/nikw"
+__projectname__ = "nikw"
+__version__ = "0.0.0"
